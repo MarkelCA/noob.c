fd3dac516b83a6ec5936aa48710edac2787ef43e
Markel <cuestaarribas.markel@gmail.com>
2021-05-12 13:16:49 +0200

    C++ Templates | Repo organization

---
+++ C++/Intermediate/Templates/template-specialization.cpp
@@ -0,0 +1,14 @@
+// A generic sort function
+template <class T>
+void sort(T arr[], int size)
+{
+	// code to implement Quick Sort
+}
+
+// Template Specialization: A function
+// specialized for char data type
+template <>
+void sort<char>(char arr[], int size)
+{
+	// code to implement counting sort
+}
